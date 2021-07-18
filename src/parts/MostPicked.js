0269387973e6d34286e77cc82a262b4409094aad
Jauharuddin <jauharnotes@gmail.com>
2021-07-18 10:21:28 +0700

    Most Picked

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -1,29 +1,39 @@
+import React from "react";
+import Button from "elements/Button";
+
 export default function MostPicked(props) {
   return (
     <section className="container">
       <h4 className="mb-3">Most Picked</h4>
-      <div className="row">
-        <div className="col-4">
-          <div className="card h-100">card 1</div>
-        </div>
-        <div className="col-8">
-          <div className="row mb-3">
-            <div className="col-6">
-              <div className="card">Card 2</div>
+      <div className="container-grid">
+        {props.data.map((item, index) => {
+          return (
+            <div className={`item column-4 ${index === 0 ? "row-2" : "row-1"}`}>
+              <div className="card card-featured">
+                <div className="tag">
+                  ${item.price}
+                  <span className="font-weight-light">per {item.unit}</span>
+                </div>
+                <figure className="img-wrapper">
+                  <img
+                    src={item.imageUrl}
+                    alt={item.name}
+                    className="img-cover"
+                  />
+                </figure>
+                <div className="meta-wrapper">
+                  <Button
+                    type="link"
+                    className="streched-link d-block text-white"
+                    href={`/properties/${item.id}`}
+                  >
+                    <h5>{item.name}</h5>
+                  </Button>
+                </div>
+              </div>
             </div>
-            <div className="col-6">
-              <div className="card">Card 3</div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-6">
-              <div className="card">Card 4</div>
-            </div>
-            <div className="col-6">
-              <div className="card">Card 5</div>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </section>
   );
